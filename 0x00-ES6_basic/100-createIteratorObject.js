09663246d03f0dc17b6dd6c4f20de575cda1f47a
Godwin Kingsley Woode <68077816+gkwoode@users.noreply.github.com>
2023-06-26 22:25:17 +0000

    ES6 Basics

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+const report = createReportObject(/* ... */);
+const iterator = createIteratorObject(report);
+
+let result = iterator.next();
+while (!result.done) {
+    const employee = result.value;
+    console.log(employee);
+    result = iterator.next();
+}

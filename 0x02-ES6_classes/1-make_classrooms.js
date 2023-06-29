fd431e45ff00cdbe7d44ae66ba0507cb96634064
gkwoode <winsleywoode@gmail.com>
2023-06-29 21:58:12 +0000

    fix: minor bug fix

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,6 +2,6 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const roomSizes = [19, 20, 34];
-  const rooms = roomSizes.map(size => new ClassRoom(size));
+  const rooms = roomSizes.map((size) => new ClassRoom(size));
   return rooms;
 }
